65f69121590be507c3b2ecbf6519eba0e0051976
Evan Stromatias <evangelos.stromatias@gmail.com>
2021-04-20 11:30:36 +0200

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,11 +1,7 @@
 #lang info
 
 (define collection "covid-invaders")
-(define deps  '("threading"
-                "2htdp/image"
-                "2htdp/universe"
-                "rackunit"
-                "base"))
+(define deps  '("threading"))
 (define pkg-desc "Space invaders variation with viruses and vaccines")
 (define version "0.1")
 (define pkg-authors '(Evan))
